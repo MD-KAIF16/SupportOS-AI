3f295f18b3d133c616f34fb0373ec1d867e847be
MD-KAIF16 <mdkaif.1608@gmail.com>
2026-06-29 15:38:11 +0530

    Added POST echo endpoint with Pydantic validation

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+# FastAPI Application
 app = FastAPI()
 
+
+# Pydantic Model
+class EchoRequest(BaseModel):
+    message: str
+
+
+# Home Route
 @app.get("/")
 def home():
     return {
         "message": "SupportOS AI Backend is Running Successfully 🚀"
+    }
+
+
+# Echo Route
+@app.post("/echo")
+def echo(request: EchoRequest):
+    return {
+        "echo": request.message
     }
